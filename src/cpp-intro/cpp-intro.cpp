84753f9664acf7b8ceaca5356be427e1fc9fc8ef
pdl39 <dhlee9111@gmail.com>
2021-04-25 19:57:36 +0900

    [cpp-intro] UPDATE: do-while loop topic added - Overall formatting updated with each topic printed out

--- src/cpp-intro/cpp-intro.cpp
+++ src/cpp-intro/cpp-intro.cpp
@@ -16,13 +16,21 @@ const double PI = 3.14;
 
 int main(int argc, char **argv) {
     // basic stdout (print)
-    cout << "Hello World" << endl;
+    cout << "***** INTRO TO C++ *****" << endl;
+    string name;
+    cout << "Please enter your name: " << endl;
+    cin >> name;
+    cout << "Hello, " << name << "! Let's start our C++ tutorial!" << endl;
+    // cout << "Hello World" << endl;
+
+    printf("\n\n");
 
     // variable declaration & initialization
     bool isTrue = true;
     char myLetter = 'A';
 
     // min & max limits for each numeric type
+    cout << "---- MIN & MAX LIMITS FOR EACH NUMERIC TYPE ----" << endl;
     cout << "Min unsigned short int: " << numeric_limits<unsigned short int>::min() << endl;
     cout << "Max unsigned short int: " << numeric_limits<unsigned short int>::max() << endl;
     cout << "Min short int: " << numeric_limits<short int>::min() << endl;
@@ -46,7 +54,10 @@ int main(int argc, char **argv) {
     cout << "Min long double: " << numeric_limits<long double>::min() << endl;
     cout << "Max long double: " << numeric_limits<long double>::max() << endl;
 
+    printf("\n\n");
+
     // size (byte) of each numeric type
+    cout << "---- SIZE (IN BYTES) OF EACH NUMERIC TYPE ----" << endl;
     printf("Size of bool: %lo byte(s)\n", sizeof(bool));
     printf("Size of char: %lo byte(s)\n", sizeof(char));
     printf("Size of unsigned short int: %lo byte(s)\n", sizeof(unsigned short int));
@@ -59,28 +70,41 @@ int main(int argc, char **argv) {
     printf("Size of double: %lo byte(s)\n", sizeof(double));
     printf("Size of long double: %lo byte(s)\n", sizeof(long double));
 
+    printf("\n");
+
     // precision of a float: 6
+    cout << "Precision of a float: 6" << endl;
     float f1 = 1.111111;
     float f2 = 1.111111;
+    printf("Sum of %f and %f = %.7f\n", f1, f2, (f1 + f2));
+
+    printf("\n");
 
     // precision of a double: 15
+    cout << "Precision of a double: 15" << endl;
     double d1 = 1.1111111111111111;
     double d2 = 1.1111111111111111;
-
-    printf("Sum of %f and %f = %.7f\n", f1, f2, (f1 + f2));
     printf("Sum of %f and %f = %.16f\n", d1, d2, (d1 + d2));
 
-    // data type assigned during compilation:
+    printf("\n");
+
+    // auto: Data Type Assigned During Compilation:
     auto whatDataType1 = 10;
     auto whatDataType2 = true;
+
+    printf("\n\n");
 
     // c -> character
     // d -> int
     // f -> float/double
     // s -> string
-    printf("%c %d %5d %.3f %s\n", 'A', 10, 5, 6.5432, "hello");
+    cout << "---- PRINTF DATA TYPE SHORTHAND ----" << endl;
+    printf("c: %c | d: %d | 5d: %5d | .3f: %.3f | s: %s\n", 'A', 10, 5, 6.5432, "hello");
+
+    printf("\n\n");
 
     // saving user input to variable
+    cout << "---- CIN: SAVING USER INPUT TO A VARIABLE ----" << endl;
     string prompt1 = "Enter a number: ";
     string num1, num2;
     cout << prompt1 << endl;
@@ -89,7 +113,7 @@ int main(int argc, char **argv) {
     cout << prompt2 << endl;
     cin >> num2;
 
-    // convert string to int
+    // Convert string to int
     int iNum1 = stoi(num1);
     int iNum2 = stoi(num2);
 
@@ -98,13 +122,15 @@ int main(int argc, char **argv) {
     printf("%d * %d = %d\n", iNum1, iNum2, (iNum1 * iNum2));
     printf("%d / %d = %d\n", iNum1, iNum2, (iNum1 / iNum2));
     printf("%d %% %d = %d\n", iNum1, iNum2, (iNum1 % iNum2));
+    printf("\n");
+    cout << "stoi converts string to int." << endl;
+    cout << "stof converts string to float." << endl;
 
-    // conditional operators
-    // : > < >= <= == !=
+    printf("\n\n");
 
-    // logical operators
-    // && || !
-
+    // Conditional Operators: > < >= <= == !=
+    // Logical Operators: && || !
+    cout << "---- OPERATORS & CONDITIONALS ----" << endl;
     string prompt3 = "Enter your age: ";
     string age1;
     cout << prompt3 << endl;
@@ -128,17 +154,26 @@ int main(int argc, char **argv) {
         printf("You are %d years old. Enjoy a relaxing life!\n", iage1);
     }
 
-    // ternary operators
+    printf("\n\n");
+
+    // Ternary Operators
+    cout << "---- TERNARY OPERATORS ----" << endl;
     bool isAdult = iage1 > 18 ? true : false;
     string sIsAdult = iage1 > 18 ? "yes" : "no";
     cout << "Can I vote? " << isAdult << endl;
     cout << "Can I vote? " << sIsAdult << endl;
 
-    // boolean: convert 1/0 to true/false
+    printf("\n\n");
+
+    // Boolean: Converting 1/0 to true/false
+    cout << "---- BOOLEAN: CONVERTING 1/0 TO TRUE/FALSE ----" << endl;
     cout.setf(ios::boolalpha);
     cout << "Can I vote? " << isAdult << endl;
 
+    printf("\n\n");
+
     // Arrays
+    cout << "---- ARRAYS ----" << endl;
     int numArr[10] = {1};       // declare array with a pre-set size. Cannot post-humously change array size.
     int numArr2[] = {1, 2, 3};  // can also set array size implicitly by initializing with a set number of elements.
     int numArr3[5] = {8, 9};
@@ -151,7 +186,10 @@ int main(int argc, char **argv) {
     int numArr4[2][2][2] = {{{1, 2}, {3, 4}}, {{5, 6}, {7, 8}}};
     cout << "numArr4[0][1][1]: " << numArr4[0][1][1] << endl;
 
+    printf("\n\n");
+
     // Vectors
+    cout << "---- VECTORS ----" << endl;
     vector<int> vNums(2);
     cout << "Vector Size: " << vNums.size() << endl;
 
@@ -160,8 +198,10 @@ int main(int argc, char **argv) {
     vNums.push_back(3);
     cout << "Vector Size: " << vNums.size() << endl;
 
+    printf("\n\n");
+
     // While Loop
-    cout << "While loop example:" << endl;
+    cout << "WHILE LOOP" << endl;
     int i = 0;
     while (i < 10) {
         if (i % 2 == 0) {
@@ -174,7 +214,28 @@ int main(int argc, char **argv) {
         i++;
     }
 
+    printf("\n\n");
+
+    // Do While Loop
+    cout << "---- DO WHILE LOOP ----" << endl;
+    srand(time(NULL));  // time declared in <ctime>
+    // srand -> a pseudo-random number generator initialized with the argument unsigned int as the 'seed'.
+    // different srand initializations with the same seed value will produce the same succession of results in subsequent calls to rand.
+    // to produce random-like effect, usually a distinct runtime value, like time from <ctime>, is used as seed.
+    int secretNum = rand() % 11;
+    int guess = 0;
+    do {
+        cout << "Guess the number: ";
+        cin >> guess;
+        if (guess > secretNum) cout << "Too Large!\n";
+        if (guess < secretNum) cout << "Too Small!\n";
+    } while (guess != secretNum);
+    cout << "Your Guess is Right! " << guess << " is the Secret Number!" << endl;
+
+    printf("\n\n");
+
     // String Stream
+    cout << "---- STRING STREAM ----" << endl;
     vector<string> words;
     stringstream ss1("words for string stream");
     string word;
@@ -190,6 +251,8 @@ int main(int argc, char **argv) {
 
     int arr1[] = {1, 2, 3};
     for (auto x : arr1) cout << "x in arr1: " << x << endl;
+
+    printf("\n\n");
 
     return 0;
 }
